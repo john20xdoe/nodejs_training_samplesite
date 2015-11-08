b06ad4ac0d54affa2188c07fbe893b0da17e9104
Lee Alexis <labermejo@eml.cc>
2015-11-08 18:19:28 +0800

    new version with request handlers module added :wq

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+//execute as: node index.js
+
+
 var server = require("./server");
 var router = require("./route");  ///strings correspond to filenames e.g "route" points to the file route.js
+var requestHandlers = require("./requestHandlers.js");
+
+var handle = {}
+handle["/"] 		= requestHandlers.start;
+handle["/start"]	= requestHandlers.start;
+handle["/upload"]	= requestHandlers.upload;
 
 
 
 
 //Start the server: this is like the method main in other languages
-server.start(router.route);
+server.start(router.route,handle);
 
-//continue at nodebeginner page 28
+//continue at nodebeginner page 28 - XXXX
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var http = require("http");
 var url = require("url");
 
-function start(route){
+function start(route,handle){
 	function onRequest(request,response){
 		var pathname = url.parse(request.url).pathname;
 
 		//this is displayed in the server console
 		console.log("Request for " +pathname + " received.");
 
-		route(pathname);
+		route(handle, pathname);
 		
 		response.writeHead(
 			200,
-			{"Content-Type":"text/plain"}
+			{"Content-Type":"text/html"}
 		);
-		response.write("Hello World");
+		response.write("Hello World<br>");
+		response.write("You are in &laquo;"+pathname+"&raquo;");
 		response.end();
 	}
 
